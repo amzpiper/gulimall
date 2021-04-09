32ac30b8500f3eea3519893c2564bf14904601bd
郭宇航 <1131717341@qq.com>
2021-04-09 09:54:54 +0800

    Spring-Cloud-Gateway

--- gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -2,7 +2,14 @@ package com.atguigu.gulimall.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 1、开启网关服务注册发现,配置nacos注册中心地址
+ * @EnableDiscoveryClient
+ * 2、
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallGatewayApplication {
 
