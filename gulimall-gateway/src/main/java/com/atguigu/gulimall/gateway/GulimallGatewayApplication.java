d58b690993bb58085950581fdbe4b7a114712d17
peinimogu <1131717341@qq.com>
2021-04-10 18:16:18 +0800

    Spring-gateway

--- gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -2,15 +2,17 @@ package com.atguigu.gulimall.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 1、开启网关服务注册发现,配置nacos注册中心地址
  * @EnableDiscoveryClient
- * 2、
+ * 2、排除数据库自动配置
+ * exclude = {DataSourceAutoConfiguration.class}
  */
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GulimallGatewayApplication {
 
     public static void main(String[] args) {
